4848b0d3c0cf98c6cbf20c6e0e7582e0a0863dea
yazakid <15012992791@163.com>
2019-03-27 17:36:14 +0800

    fix

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -37,18 +37,19 @@ class CommentInput extends Component {
   }
 
   render() {
+    const {username, content} = this.state;
     return (
       <div className='comment-input'>
         <div className='comment-field'>
           <span className='comment-field-name'>用户名：</span>
           <div className='comment-field-input'>
-            <input value={this.state.username} onChange={this.handleInputClick.bind(this)}/>
+            <input value={username} onChange={this.handleInputClick.bind(this)}/>
           </div>
         </div>
         <div className='comment-field'>
           <span className='comment-field-name'>评论内容：</span>
           <div className='comment-field-input'>
-            <textarea value={this.state.content} onChange={this.handleTextClick.bind(this)}/>
+            <textarea value={content} onChange={this.handleTextClick.bind(this)}/>
           </div>
         </div>
         <div className='comment-field-button'>
